25279c24c3818d58b7cb5663ca781de3b8bffa74
MaikeRob <maike.santana2@gmail.com>
2024-09-29 22:30:52 -0300

    Começando a implementar Classes

---
+++ src/InputLayer.java
@@ -0,0 +1,8 @@
+public class InputLayer extends Layer{
+
+    private float bias;
+
+    public InputLayer() {}
+
+
+}
---
+++ src/Model.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.event.MethodExitEvent;
+
+public class Model {
+
+    private InputLayer inputLayer;
+    private HideLayer[] hideLayers;
+    private ExitLayer exitLayer;
+    int hideLayersSize;
+    int[] input;
+
+
+    public Model() {}
+
+
+}
